7736e4605465d91fb2c5613324949f59146bbffe
chetansisodiya-design <chetan.sisodiya@gmail.com>
2026-06-23 00:41:54 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> H;
+
+        for(const auto& word: strs) {
+            string temp = word;
+            sort(temp.begin(), temp.end());
+
+            H[temp].push_back(word);
+        }
+
+        vector<vector<string>> result;
+        for(auto &p: H) {
+            result.push_back(p.second);
+        }
+        return result;
+    }
+};
